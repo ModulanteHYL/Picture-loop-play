573531335c6afd7d5d4be8ef140237067c5877c8
ModulanteHYL <13317634939@163.com>
2019-05-07 10:37:48 +0800

    循环播放2.0版本

--- js/main.js
+++ js/main.js
@@ -1,42 +1,66 @@
-initStatus();
-var i = 1;
-var timer=setInterval(() => {
-    leave(getImgNode(i)).one('transitionend', (e) => {
-        waiting($(e.currentTarget));
+let $images = $('.images');
+let $imgs = $('.images').children('img');
+let current = 1;
+init();
+let timer =loopStart();
+    $(loopPlay).on('mouseenter', function () {
+        loopStop(timer);
+    }).on('mouseleave', function () {
+        timer = loopStart();
     });
-    if (i === 4) { i = 0; }
-    enter(getImgNode(i + 1));
-    i += 1;
-}, 3000);
-$(document).on('visibilitychange',function(){
-    if(document.hidden){
-        window.clearInterval(timer);
-    }else{
-        timer=setInterval(() => {
-            leave(getImgNode(i)).one('transitionend', (e) => {
-                waiting($(e.currentTarget));
-            });
-            if (i === 4) { i = 0; }
-            enter(getImgNode(i + 1));
-            i += 1;
-        }, 3000);
+$(btnList).on('click', 'button', function (e) {
+    let id = $(e.currentTarget).attr('id');
+    let index;
+    if (id === 'previous') {//上一张
+        index = Modified_value(current - 1);
+    } else if (id === 'next') {//下一张
+        index = Modified_value(current + 1);
+    } else {
+        index = Number($(e.currentTarget).attr('id'));
     }
+    nextPicture(index);
 });
-
-
-function initStatus() {
-    $('.images>img:nth-child(1)').addClass('enter')
-        .siblings().addClass('waiting');
+function init() {//初始化
+    let firstPicture = $imgs.eq(0).clone(true);
+    let lastPicture = $imgs.eq($imgs.length - 1).clone(true);
+    $images.append(firstPicture);//在第一张的前面加入最后一张图
+    $images.prepend(lastPicture);//在最后一张的后面加入第一张图
 }
-function getImgNode(index) {
-    return $(`.images>img:nth-child(${index})`);
+function nextPicture(index) {
+    if (current === 1 && index === $imgs.length) {//当前为第一张，想跳到最后一张
+        $images.css({ transform: `translateX(${(-current + 1) * 400}px)` });
+        $images.one('transitionend', function () {
+            $images.css({ transition: 'none' }).hide()
+                .css({ transform: `translateX(${-index * 400}px)` }).show()
+                .css({ transition: 'all 0.5s' });
+        });
+    } else if (current === $imgs.length && index === 1) {//当前为最后一张，想跳到第一张
+        $images.css({ transform: `translateX(${(-current - 1) * 400}px)` });
+        $images.one('transitionend', function () {
+            $images.css({ transition: 'none' }).hide()
+                .css({ transform: `translateX(${-index * 400}px)` }).show()
+                .css({ transition: 'all 0.5s' });
+        });
+    } else {
+        $images.css({ transform: `translateX(${-index * 400}px)` });
+    }
+    current = index;
 }
-function leave($node) {
-    return $node.addClass('leave').removeClass('enter')
+function loopStart() {//循环播放开始
+    return setInterval(function () {
+        let index = Modified_value(current + 1);
+        nextPicture(index);
+    }, 2000);
 }
-function waiting($node) {
-    return $node.addClass('waiting').removeClass('leave');
+function loopStop(timer){//循环播放结束
+    window.clearInterval(timer);
 }
-function enter($node) {
-    return $node.addClass('enter').removeClass('waiting');
+function Modified_value(index) {//让index的值永远是1、2、3、4
+    let num = index;
+    if (num < 1) {
+        num = $imgs.length;
+    } else if (num > $imgs.length) {
+        num = 1;
+    }
+    return num;
 }
